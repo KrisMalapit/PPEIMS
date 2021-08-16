3afc0362caf18dca370380c37eda84fdf019b22f
Kristoffer Malapit <krismalapit@yahoo.com>
2021-08-16 14:03:36 +0800

    4

--- PPEIMS/Controllers/RequestsController.cs
+++ PPEIMS/Controllers/RequestsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
 using DNTBreadCrumb.Core;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +22,113 @@ namespace PPEIMS.Controllers
         {
             this.SetCurrentBreadCrumbTitle("Request");
             return View();
+        }
+        [HttpPost]
+        public ActionResult getData()
+        {
+            string strFilter = "";
+            try
+            {
+
+
+                var draw = Request.Form["draw"].FirstOrDefault();
+                var start = Request.Form["start"].FirstOrDefault();
+                var length = Request.Form["length"].FirstOrDefault();
+                var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+                var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+                var searchValue = Request.Form["search[value]"].FirstOrDefault();
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+                int recordsTotal = 0;
+
+
+                for (int i = 0; i < 3; i++)
+                {
+                    string colval = Request.Form["columns[" + i + "][search][value]"];
+                    if (colval != "")
+                    {
+                        colval = colval.ToUpper();
+                        string colSearch = Request.Form["columns[" + i + "][name]"];
+
+
+
+                        if (strFilter == "")
+                        {
+
+                            strFilter = colSearch + ".ToString().ToUpper().Contains(" + "\"" + colval + "\"" + ")";
+
+                        }
+                        else
+                        {
+                            strFilter = strFilter + " && " + colSearch + ".ToString().ToUpper().Contains(" + "\"" + colval + "\"" + ")";
+                        }
+
+                    }
+                }
+
+
+                if (strFilter == "")
+                {
+                    strFilter = "true";
+                }
+
+
+
+                int recCount =
+
+                _context.Requests
+                .Where(a => a.Status == "Active")
+
+                .Where(strFilter)
+                .Count();
+
+                recordsTotal = recCount;
+                int recFilter = recCount;
+
+
+
+                var v =
+
+               _context.Requests
+              .Where(a => a.Status == "Active")
+              .Where(strFilter)
+              .Skip(skip).Take(pageSize)
+              .Select(a => new
+              {
+                  a.CreatedDate,
+                  a.ReferenceNo,
+                  a.CreatedBy,
+                  a.DocumentStatus,
+                  a.Id
+                
+              });
+
+
+
+
+                bool desc = false;
+                if (sortColumnDirection == "desc")
+                {
+                    desc = true;
+                }
+                v = v.OrderBy(sortColumn + (desc ? " descending" : ""));
+
+
+
+                if (pageSize < 0)
+                {
+                    pageSize = recordsTotal;
+                }
+
+
+                var data = v;
+                var jsonData = new { draw = draw, recordsFiltered = recFilter, recordsTotal = recordsTotal, data = data };
+                return Ok(jsonData);
+            }
+            catch (Exception ex)
+            {
+                return Json(ex);
+            }
         }
     }
 }
